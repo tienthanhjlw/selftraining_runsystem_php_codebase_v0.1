f524d434e2b002f2ab084d2a49a0ee0eb8a07922
ngoandt <ngoandt@runsystem.net>
2017-06-21 09:44:26 +0700

    use controller with config

--- app/controller/NewsController.php
+++ app/controller/NewsController.php
@@ -2,11 +2,14 @@
 
 if (!defined('PATH_SYSTEM')) die ('Bad requested !');
 
-class NewsController
+class NewsController extends Controller
 {
     public function index()
     {
         echo '<h1>Hello</h1>';
+        echo '<h3>Token: csrf_token_name: ' . $this->config->item('csrf_token_name') . '</h3>';
+        $this->config->setItem('csrf_token_name', 'new_token');
+        echo '<h3>Token: csrf_token_name(changed): '  . $this->config->item('csrf_token_name') . '</h3>';
     }
 
     public function detail()
